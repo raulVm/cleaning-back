60503bc3f85b319e4432c4dfc2c1ea44a1292297
raulVM <daul_94@hotmail.com>
2024-02-19 17:15:55 -0600

    primer push

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre_completo');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('usuario');
+            $table->string('telefono');
+            $table->string('estatus');
+            $table->string('rol');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
